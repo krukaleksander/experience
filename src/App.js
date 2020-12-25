68eaff5d02cd6e31121bb122845a45716a7436a2
Alek Kruk <kruk.aleksander@hotmail.com>
2020-12-25 07:10:49 +0100

    initial commit, modyfy css, create components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+
+  return <h1>We start =)</h1>
+}
+
+export default App;
---
+++ src/Job.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Job = () => {
+    return <h1>Job section</h1>
+};
+
+export default Job;
